4b4387dfe77662c638259fe4a8fff0073ebbc065
LeenaSai <66858274+LeenaSai@users.noreply.github.com>
2021-08-31 21:17:57 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -1,6 +1,6 @@
 package hello;
 
-public class Greeter {
+//public class Greeter {
   public String sayHello() {
     return "Hello world- This is Leena Sai your Trainer !! Hi or Bye is the string needed here.Testing Pipeline";
   }
